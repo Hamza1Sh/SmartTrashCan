6eb7c4d7d2b19397158aadbba9194c899f4045f4
Hamza1Sh <34596435+Hamza1Sh@users.noreply.github.com>
2018-03-25 11:47:56 -0400

    Update sensor2.ino

--- sensor2.ino
+++ sensor2.ino
@@ -1,34 +1,3 @@
-//int ledPin =0; // Add the onboard LED on pin 13.
-//int trigPin = 16; // Add the Trig pin on pin 10.
-//int echoPin = 15; // Add the ECHO pin on pin 9.
-//int duration, distance; // Add types 'duration' and 'distance'.
-//void setup()
-//{
-//Serial.begin(9600);
-//pinMode (ledPin, OUTPUT); // The LED must be controlled by Arduino, it means it is an output type.
-//pinMode (trigPin, OUTPUT);// Same as above, the TRIG pin will send the ultrasonic wave.
-//pinMode (echoPin, INPUT); // The ECHO pin will recieve the rebounded wave, so it must be an input type.
-//}
-//void loop()
-//{
-//digitalWrite (ledPin, LOW); // Here, LOW means off and HIGH means on.
-//digitalWrite (trigPin, HIGH);
-//delay(50);
-//digitalWrite (trigPin, LOW);
-//duration=pulseIn(echoPin,HIGH);
-//distance=(duration/2)/29.1;
-//if(distance <=3) // If the sensor detects an obstacle less than 30 cm in distance, the LED will start to blink.
-//digitalWrite (ledPin, LOW);
-//
-//delay(50);
-//if(distance >=3)// If no obstacle is there within 30 cm, the Led should turn off.
-//digitalWrite (ledPin, HIGH);
-//delay(50);
-//Serial.print("cm"); 
-//Serial.println(distance);
-//}
-
-
 int out=12;
 int in=13;
 
